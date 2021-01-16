3f304b74a0bc60d7105d64bbdf07650d7e281b9e
Tariq Almazyad <Tariq.Almazyad@gmail.com>
2021-01-16 22:09:13 +0300

    this is the new project

---
+++ git pracitce/Shared/git_pracitceApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_pracitceApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct git_pracitceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
